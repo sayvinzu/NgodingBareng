4755f843139bb445b1e0959f83c602e7c093e1e2
sayvinzu <syaifulamriyahya@gmail.com>
2017-10-08 02:01:53 +0700

    add splash screen splash screen fix

--- app/src/main/java/com/example/sayvinzu/ngodingbareng/SplashScreen.java
+++ app/src/main/java/com/example/sayvinzu/ngodingbareng/SplashScreen.java
@@ -1,13 +1,40 @@
 package com.example.sayvinzu.ngodingbareng;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int splashInterval = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i); // menghubungkan activity splashscren ke main activity dengan intent
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
+
+
 }
