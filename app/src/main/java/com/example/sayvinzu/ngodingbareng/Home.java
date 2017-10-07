60a8de252c35ad5cb276003545429ef81abc362a
sayvinzu <syaifulamriyahya@gmail.com>
2017-10-08 02:35:53 +0700

    add splash screen splash screen fix navigation buttom

---
+++ app/src/main/java/com/example/sayvinzu/ngodingbareng/Home.java
@@ -0,0 +1,51 @@
+package com.example.sayvinzu.ngodingbareng;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_scadule:
+                    mTextMessage.setText(R.string.title_scadule);
+                    return true;
+                case R.id.navigation_add:
+                    mTextMessage.setText(R.string.title_add);
+                    return true;
+                case R.id.navigation_filter:
+                    mTextMessage.setText(R.string.title_filter);
+                    return true;
+                case R.id.navigation_user:
+                    mTextMessage.setText(R.string.title_user);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
